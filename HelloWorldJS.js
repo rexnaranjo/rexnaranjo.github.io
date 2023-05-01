1cb382d54eb8744c4a9cb61654f9aa1ea6c88d8c
Rex Naranjo <rex.pangan.naranjo@gmail.com>
2023-05-02 00:11:33 +0800

    testing my Github connection

---
+++ HelloWorldJS.js
@@ -0,0 +1,8 @@
+document.getElementById("button").onclick = function () {
+	document.getElementById("confirm").innerHTML = "Order placed"
+}
+
+document.getElementById("button1").onclick = function () {
+	document.getElementById("button1").innerHTML = "ok"
+}
+
